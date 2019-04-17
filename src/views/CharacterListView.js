1c0fb3994012266fd16869c6dacf05f575ebe638
JamesB <jblazer1@yahoo.com>
2019-04-17 13:55:33 -0700

    Working but not sure why

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -27,19 +27,32 @@ class CharacterListView extends React.Component {
   }
 }
 
-// our mapStateToProps needs to have two properties inherited from state
-const mapStateToProps = state => ({
-  characters: state.characters,
-  error: state.error
-});
-// the characters and the fetching boolean
+// // our mapStateToProps needs to have two properties inherited from state
+// const mapStateToProps = state => ({
+//   characters: state.characters,
+//   error: state.error
+// });
+// // the characters and the fetching boolean
+// export default connect(
+//   // null /* mapStateToProps replaces null here */,
+//   mapStateToProps,
+//   {
+//     /* action creators go here */
+//     characters: state.characters,
+//     error: state.error,
+//     isFetching: state.isFetching
+//   }
+// )(CharacterListView);
+
+const mapStateToProps = state => {
+  return {
+    characters: state.charsReducer.characters,
+    isFetching: state.isFetching,
+    error: state.error
+  };
+};
+
 export default connect(
-  // null /* mapStateToProps replaces null here */,
   mapStateToProps,
-  {
-    /* action creators go here */
-    characters: state.characters,
-    error: state.error,
-    isFetching: state.isFetching
-  }
+  { getChars }
 )(CharacterListView);
