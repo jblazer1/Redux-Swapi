405014d102ee31e45887a058a73aef158c07f598
JamesB <jblazer1@yahoo.com>
2019-04-17 14:03:36 -0700

    Expand info printed in Character.js

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Character = props => {
-  return <li>{props.character.name}</li>;
+  return (
+    <div>
+      <ul>
+        <li>NAME: {props.character.name}</li>
+        <li>HEIGHT: {props.character.height}</li>
+        <li>MASS: {props.character.mass}</li>
+        <li>EYE COLOR: {props.character.eye_color}</li>
+        <li>HAIR COLOR: {props.character.hair_color}</li>
+        <li>GENDER: {props.character.gender}</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Character;
